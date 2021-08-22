a11fdf0ed2f2b02ab3a63ab9bc6f0f822eff6839
Evelyn <evelynpinho@gmail.com>
2021-08-22 13:35:29 -0300

    feat: refactor

---
+++ assets/scripts/glide.js
@@ -0,0 +1,32 @@
+import Glide from '@glidejs/glide';
+
+function mountGlide() {
+  const doc = document.querySelector('.glide');
+
+  if (doc) {
+    const glide = new Glide(doc, {
+      type: 'slider',
+      startAt: 0,
+      perView: 3,
+      hoverpause: false,
+      gap: 24,
+      bound: true,
+      breakpoints: {
+        1280: {
+          perView: 3,
+        },
+        1000: {
+          perView: 2,
+        },
+        670: {
+          perView: 1,
+        },
+      },
+    });
+    glide.mount();
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  mountGlide();
+});
